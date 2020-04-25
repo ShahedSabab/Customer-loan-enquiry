68c0b1f819d57121bdd3528057c9915541982b38
ShahedSabab <59721350+ShahedSabab@users.noreply.github.com>
2020-04-25 06:15:40 -0500

    add files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn CustomerLoanDjango.wsgi
---
+++ myapi/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('myapi', views.approvalView)
+urlpatterns = [
+    path('', views.cxcontact, name='cxform'),
+    # path('api/', include(router.urls)),
+    # path('status/', views.approveReject),
+]
